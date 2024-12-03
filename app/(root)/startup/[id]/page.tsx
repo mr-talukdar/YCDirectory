118b5d220de711b9dc054a91457b216531ae9e43
Rahul Talukdar <42152385+mr-talukdar@users.noreply.github.com>
2024-12-03 17:55:46 +0530

    Added amrkdown

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -6,14 +6,20 @@ import {formatDate} from "@/lib/utils";
 import {notFound} from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
+import markdownit from 'markdown-it'
+
+const md=markdownit();
 
 export const experimental_ppr=true;
+
 
 const Page =async  ({params}:{params: Promise<{id:string}>}) => {
     const {id}= await params;
     const data = await client.fetch(STARTUP_DETAILS_QUERY, {id})
 
     if(!data) return notFound();
+
+    const parsedData=md.render(data.pitch);
 
     return (
         <>
@@ -42,9 +48,18 @@ const Page =async  ({params}:{params: Promise<{id:string}>}) => {
                     <h3 className={'text-30-bold'}>
                         Pitch Details
                     </h3>
+                    {parsedData ? (
+                        <article className={'prose max-w-4xl font-work-sans break-all'} dangerouslySetInnerHTML={{__html: parsedData}}/>
+                    )
+                     : (
+                         <p className={'no-result'}> No Pitch yet</p>
+                        )
+                    }
                 </div>
-            </section>
+                <hr className={'divider'}/>
 
+            </section>
+            {/*TODO: Editor SELECTED Startups*/}
         </>
     )
 }
